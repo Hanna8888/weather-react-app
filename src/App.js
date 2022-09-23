fa40efd5da4bcc82ffc6835317f15162c3ff159d
Anna <alexanet855@gmail.com>
2022-09-23 14:03:24 +0300

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+const api = axios.create({
+	baseURL: 'https://api.openweathermap.org/data/2.5'
+});
+
+function App() {
+
+	const [query, setQuery] = useState('');
+  	const [weather, setWeather] = useState({});
+
+	const search = () => {
+		api.get(`/weather?q=${query}&appid=45a3d7d27c9b92a35c8c68488f9ffd5b&units=metric`)
+		.then(result => {
+			setWeather(result.data);
+			setQuery('');
+			console.log(result.data);
+		});
+	}
+
+	return (
+		<div className='bg-img'>
+			<div className='container'>
+				<h1 className='text-light pt-5'>Weather App</h1>
+				<div className='row mt-5'>
+					<div className='col-md-6'>
+						<input
+							type='text'
+							className='form-control rounded-0'
+							placeholder='Search...'
+							onChange={e => setQuery(e.target.value)}
+							value={query} 
+						/>
+					</div>
+					<div className='col-md-6 text-center text-lg-start mt-3 mt-lg-0'>
+						<button type='submit' className='btn bg-btn rounded-0' onClick={search}>Search</button>
+					</div>
+					{(typeof weather.main != 'undefined') ? (
+						<div className='col-md-12 mt-3 mt-lg-4 d-flex justify-content-center justify-content-lg-start'>
+							<div className='card card-custom rounded-0' style={{width: 288}}>
+								<div className='card-body shadow-lg'>
+									<h5 className='card-title'>{weather.name}, {weather.sys.country}</h5>
+									<h6 className='card-subtitle mb-2'>{Math.round(weather.main.temp)}°c</h6>
+									<p className='card-text'>{weather.weather[0].main}</p>
+								</div>
+							</div>
+						</div>
+					) : ('')}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
